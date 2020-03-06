a3f8dd2e4fe90d7b6fb59e623233921da4db7cf4
Victor <55414928+victorleles12@users.noreply.github.com>
2020-03-05 22:23:48 -0300

    Mike
    Teste sofrido 01

---
+++ main.js
@@ -0,0 +1,8 @@
+let botao = document.getElementById('botao') 
+let tarefa = document.querySelector('#entrada');
+
+function registrar() {
+    let tr = document.getElementById("tabela-corpo").createElement("tr");
+      console.log(tarefa.value);
+  }
+  
